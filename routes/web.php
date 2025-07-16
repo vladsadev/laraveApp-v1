366964db456d0d759357b5ff520f0bf9245d8fe7
vladsadev <vladsa@outlook.com>
2025-07-16 00:00:46 -0400

    Route::resource

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,7 @@ use App\Models\Job;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-    Route::get('/jobs/{job}/edit', 'edit');
-});
-
+Route::resource('jobs',JobController::class);
 
 Route::get('/contact', function () {
     return view('contact');
